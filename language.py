cde1d4c7f9d11d8b42477474d1ca702c2d0e0c43
Jack Hale <misterjackhale@gmail.com>
2018-12-02 11:31:37 -0600

    added random sampling, better cleanup, no repeat productions

--- language.py
+++ language.py
@@ -8,45 +8,41 @@ from random import randint
 import os
 
 if os.name == "nt":
-    tweet_text_data = dc.get_cleaned_up_tweet_text_data(filename="trump_tweets_ansi.txt",
-                                                        get_text=dc.get_tweet_text_data_ansi)
+    tweets = dc.get_cleaned_up_tweet_text_data(filename="trump_tweets_ansi.txt",
+                                               get_text=dc.get_tweet_text_data_ansi)
 else:
-    tweet_text_data = dc.get_cleaned_up_tweet_text_data()
+    tweets = dc.get_cleaned_up_tweet_text_data()
 
 max_tweets = 2934
-tweets = tweet_text_data.split("\n")
+# tweets = tweet_text_data.split(".")
+total_num_tweets = len(tweets)
 
-corpus = dc.create_corpus_from_text_data(text_data=tweet_text_data,
-                                         max_lines=max_tweets)
 quote = "Incredible to be with our GREAT HEROES today in California."
 parser = CoreNLPParser(url='http://localhost:9000')
 
 sentences = []
-tweet_range = 10
-upper = randint(tweet_range, len(tweets))
-lower = upper - tweet_range
-for i in range(lower, upper):
-    print(i)
-    sentences += parser.raw_parse(tweets[i])
-#sentences += parser.raw_parse(tweets[0])
-
-productions = []
-min_num_tweets = 30
-end_index = randint(min_num_tweets, len(tweets))
-start_index = end_index - min_num_tweets
-for i in range(start_index, end_index):
-    print(f"Parsing tweet number {i}")
-    sentence = parser.raw_parse(tweets[i])
+all_productions = []
+used_tweet_nums = []
+sample_num_tweets = 25
+for count in range(sample_num_tweets):
+    random_index = randint(0, total_num_tweets)
+    while random_index in used_tweet_nums:
+        random_index = randint(0, total_num_tweets)
+    used_tweet_nums.append(random_index)
+    print(f"Parsing tweet number {random_index}:")
+    print(tweets[random_index])
+    sentence = parser.raw_parse(tweets[random_index])
     sentences += sentence
 for sentence in sentences:
-    productions += sentence.productions()
+    productions = sentence.productions()
+    if productions not in all_productions:
+        all_productions += productions
 
-grammar = nltk.grammar.induce_pcfg(nltk.Nonterminal('ROOT'), productions)
+grammar = nltk.grammar.CFG(nltk.Nonterminal('ROOT'), all_productions)
 print(grammar)
 
-
 new_tweets = []
-for sentence in generate(grammar, depth=10, n=1000):
+for sentence in generate(grammar, depth=10, n=25):
     string = ' '.join(sentence)
     if not string in new_tweets:
         new_tweets.append(string)
