13858bac124060d69aca8c07d098623d936c980e
Matt Madden <matthewkayin@gmail.com>
2018-12-02 12:15:55 -0600

    Made a generate function
    Signed-off-by: Matt Madden <matthewkayin@gmail.com>

--- language.py
+++ language.py
@@ -4,8 +4,58 @@ import data_collection as dc
 from nltk.grammar import CFG
 from nltk.parse import CoreNLPParser
 from nltk.parse.generate import generate
-from random import randint
+from random import randint, choice
 import os
+
+def isRepetitive(sentence):
+    words = sentence.split(' ')
+    numberOfRepeats = 0
+    for i in range(0, len(words)):
+        count = words.count(words[i])
+        if count > 2:
+            numberOfRepeats += 1
+    if numberOfRepeats > 3:
+        return True
+    else:
+        return False
+
+def createSentence(productions, start):
+    newString = []
+    #print("start = " + str(start))
+    for token in start:
+        #print("for token " + str(token))
+        validProductions = []
+        for production in productions:
+            if str(production.lhs()) == str(token):
+    #            print("valid production found! = " + str(production))
+                validProductions.append(production)
+        if not len(validProductions) == 0:
+            toUse = choice(validProductions)
+    #        print("chosen to use production " + str(toUse))
+            rhs = toUse.rhs()
+            for t in rhs:
+    #            print("adding to string token " + str(t))
+                newString.append(t)
+        else:
+            newString.append(token)
+    #print("newString = " + str(newString))
+    finished = True
+    for token in newString:
+        if str(token).isalpha():
+            if str(token).upper() == str(token):
+                finished = False
+                break
+    if finished:
+        rVal = ""
+        for token in newString:
+            if str(token) == ".":
+                rVal += str(token)
+            else:
+                rVal += str(token)
+                rVal += " "
+        return rVal
+    else:
+        return createSentence(productions, newString)
 
 if os.name == "nt":
     tweet_text_data = dc.get_cleaned_up_tweet_text_data(filename="trump_tweets_ansi.txt",
@@ -22,33 +72,24 @@ quote = "Incredible to be with our GREAT HEROES today in California."
 parser = CoreNLPParser(url='http://localhost:9000')
 
 sentences = []
-tweet_range = 10
-upper = randint(tweet_range, len(tweets))
-lower = upper - tweet_range
-for i in range(lower, upper):
-    print(i)
-    sentences += parser.raw_parse(tweets[i])
-#sentences += parser.raw_parse(tweets[0])
-
 productions = []
-min_num_tweets = 30
+min_num_tweets = 10
 end_index = randint(min_num_tweets, len(tweets))
 start_index = end_index - min_num_tweets
 for i in range(start_index, end_index):
     print(f"Parsing tweet number {i}")
     sentence = parser.raw_parse(tweets[i])
     sentences += sentence
+productions = []
 for sentence in sentences:
-    productions += sentence.productions()
+    if not sentence.productions() in productions:
+        productions += sentence.productions()
 
-grammar = nltk.grammar.induce_pcfg(nltk.Nonterminal('ROOT'), productions)
-print(grammar)
-
-
+for p in productions:
+    print(p)
 new_tweets = []
-for sentence in generate(grammar, depth=10, n=1000):
-    string = ' '.join(sentence)
-    if not string in new_tweets:
-        new_tweets.append(string)
-        print("Generated unique tweet:")
-        print(string)
+for i in range(0, 10):
+    nt = createSentence(productions, ['ROOT'])
+    new_tweets.append(nt)
+for nt in new_tweets:
+    print(nt)
