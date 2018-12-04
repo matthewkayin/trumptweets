5e5384be9cc81d106a53dc7c487a8c8a0ac7588b
Jack Hale <misterjackhale@gmail.com>
2018-12-03 20:35:13 -0600

    added load from file

---
+++ textgen_from_save.py
@@ -0,0 +1,28 @@
+from textgenrnn import textgenrnn
+import sys
+
+# python textgen_from_save.py number_of_tweets seed_string degree_freedom
+#                               int             string      float
+print(sys.argv)
+filename, num_tweets, seed, degree_freedom = sys.argv
+
+if seed == "None":
+    seed = None
+
+tweet_length = 140
+textgen = textgenrnn()
+textgen.reset()
+
+# textgen.train_from_file("trump_tweets.txt", num_epochs=1)
+textgen.load("premade_weights.hdf5")
+
+output = textgen.generate(int(num_tweets),
+                          prefix=seed,
+                          temperature=float(degree_freedom),
+                          return_as_list=True,
+                          max_gen_length=tweet_length)
+
+print("========Training Complete========")
+for tweet in output:
+    print(f"Tweet before grammar check:\n {tweet}\n")
+
